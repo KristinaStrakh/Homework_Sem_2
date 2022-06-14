2913d6c97101519a2b17c421cada124d05cf37c0
Kristina Strakh <kris.strakg@gmail.com>
2022-06-14 17:28:23 +0300

    Add Tasks

--- HomeW_2/Tasks/Program.cs
+++ HomeW_2/Tasks/Program.cs
@@ -63,4 +63,27 @@ else
     Console.WriteLine ("Вы ввели не день недели");
 }*/
 
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
 
+int number = new Random().Next(1,100000);
+Console.WriteLine(number);
+int ThirdNumb = 0;
+
+while (number > 1000 )
+{
+    number = number / 10;
+
+     if (number < 1000 && number > 100)
+    {
+     ThirdNumb = number % 10;
+     Console.WriteLine(ThirdNumb);
+    }
+}
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
